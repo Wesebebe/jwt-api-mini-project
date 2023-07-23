76f49196e9ae52cae70047adf414a6a7a57e0421
Reagan <rokhanga@gmail.com>
2023-07-23 16:20:37 +0300

    fix errors in login action and add route to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,18 +12,18 @@ class UsersController < ApplicationController
 
     def login
         @user = User.find_by(username: user_params[:username])
-
+    
         if @user && @user.authenticate(user_params[:password])
             token = encode_token({ user_id: @user.id })
             render json: { user: @user, token: token }, status: :created
         else
-            render json: {error: "Invalid username or password"}, status: :unprocessable_entity
+            render json: {error: "error"}, status: :unprocessable_entity
         end
     end
 
     private 
 
     def user_params
-        params.permit(:username, :password)
+        params.require(:user).permit(:username, :password)
     end
 end
